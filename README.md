51f7dead13a4b5dbf13835c24be4d69234645104
Levy Andrew <85814374+levyandrew16@users.noreply.github.com>
2021-09-06 03:24:53 -0400

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # DEPLOY04_Custom-Calculator
+
+This a simple Calculator that add, subtract, multiply and divide, three input numbers. 
---
+++ application.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template, request
+
+application = app = Flask(__name__)
+
+
+@app.route('/')
+def main():
+    return render_template("cal.html")
+
+
+@app.route("/calculate", methods=['POST'])
+def calculate():
+    num1 = request.form['num1']
+    num2 = request.form['num2']
+    num3 = request.form['num3']
+    operation = request.form['operation']
+
+    if operation == 'add':
+        result = float(num1) + float(num2) + float(num3)
+        return render_template('cal.html', result=result)
+
+    elif operation == 'subtract':
+        result = float(num1) - float(num2) - float(num3)
+        return render_template('cal.html', result=result)
+
+    elif operation == 'multiply':
+        result = float(num1) * float(num2) * float(num3)
+        return render_template('cal.html', result=result)
+
+    elif operation == 'divide':
+        result = float(num1) / float(num2) / float(num3)
+        return render_template('cal.html', result=result)
+    else:
+        return render_template('cal.html')
